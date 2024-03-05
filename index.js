a0239815d597c723ec9b87ed7b22f224cd9052f1
sac1010 <sachingirish101@gmail.com>
2024-03-05 23:05:29 +0530

    cors

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
-require("dotenv").config()
+require("dotenv").config();
 const mongoose = require("mongoose");
 const userRoutes = require("./src/routes/userRoutes");
-
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.mongoUri);
 
